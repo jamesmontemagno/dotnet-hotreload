cca7ffda8bcfaf9e870523785cb8d314fc0e78ae
James Montemagno <james.montemagno@gmail.com>
2021-11-10 15:41:50 -0800

    Add project files.

---
+++ HotReload5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace HotReload5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
+
+/*while(true)
+{
+	Console.WriteLine("Hello World!");				
+	Thread.Sleep(1000);
+}*/
+
+//var id = System.Diagnostics.Process.GetCurrentProcess().Id;
---
+++ HotReload6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while(true)
+{
+    OutputPid();
+}
+
+
+void OutputPid() 
+{
+    Console.WriteLine($"Hello World! {Environment.ProcessId}");
+    Thread.Sleep(1000);
+}
